2a67350c9bce4d339abf34e14703fdc8f81bd344
pyledw <pyledw@gmail.com>
2026-07-22 17:25:07 -0500

    update

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -17,7 +17,6 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
 #include <obs-module.h>
-#include <obs-version.h>
 #include <plugin-support.h>
 #include <obs-frontend-api.h>
 
